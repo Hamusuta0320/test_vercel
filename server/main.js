433613aed87fa4476a957e5ea929cb7137178108
Hamusuta <2326955513@qq.com>
2022-08-03 21:10:23 +0800

    delete

--- server/main.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const port = 80
-const app = express()
-app.get('/', (req, res)=> {
-  res.send('Hello Wo')
-})
-app.listen(port, ()=> {
-  console.log(`Express start at ${port}`)
-})
